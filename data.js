6f85c48a7342efb7ea051ba1664bfa7bf0068c05
Ammar Bdr. Kunwar <104131073+ammard3v@users.noreply.github.com>
2024-08-26 20:45:50 +0545

    Add files via upload

---
+++ data.js
@@ -0,0 +1,89 @@
+// Define default subjects for each semester
+export const defaultSubjects = {
+  1: [
+    "English",
+    "Mathematics I",
+    "Digital Logic Systems",
+    "Computer Fundamentals and Application",
+    "Programming Logic and Techniques",
+  ],
+  2: [
+    "Business Communication",
+    "Mathematics II",
+    "Financial Accounting I",
+    "Programming Language in C",
+    "Fundamentals of Electrical and Electronics",
+    "Project I",
+  ],
+  3: [
+    "Object Oriented Programming in C++",
+    "Data Structure and Algorithms",
+    "System Analysis and Design",
+    "Financial Accounting II",
+    "Microprocessor",
+  ],
+  4: [
+    "Numerical Methods",
+    "Visual Programming",
+    "Operating Systems",
+    "Database Management System",
+    "Computer Graphics and Multimedia Technology",
+    "Project II",
+  ],
+  5: [
+    "Computer Architecture",
+    "Java Programming",
+    "Web Technology I",
+    "Mathematical Foundation and Computer Science",
+    "Software Engineering",
+  ],
+  6: [
+    "Data Communication and Computer Network",
+    "Fundamentals of Probability and Statistics",
+    "Applied Economics",
+    "Organization and Management",
+    "Web Technology II",
+    "Project III",
+  ],
+  7: [
+    "E-Business",
+    "Simulation and Modeling",
+    "Linux",
+    "Internship",
+    "Elective I(.NET Programming)",
+  ],
+  8: [
+    "Mobile Application and Development Technology",
+    "Management Information System",
+    "Elective II",
+    "Project IV",
+  ],
+};
+
+// Define default credits for each semester
+export const defaultCredits = {
+  1: [3, 3, 3, 3, 3],
+  2: [3, 3, 3, 3, 3, 1],
+  3: [3, 3, 3, 3, 3],
+  4: [3, 3, 3, 3, 4, 2],
+  5: [3, 3, 3, 3, 3],
+  6: [3, 3, 3, 3, 3, 3],
+  7: [3, 3, 3, 3, 3],
+  8: [3, 3, 3, 4],
+};
+
+// Define grade options
+export const gradeOptions = [
+  "A",
+  "A-",
+  "B+",
+  "B",
+  "B-",
+  "C+",
+  "C",
+  "C-",
+  "D+",
+  "D",
+  "F",
+];
+
